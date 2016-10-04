4e76b75cdc1136b9d389b312d154e07665e15b02
alexmacaulay <alex@macaulay.me.uk>
2016-10-04 10:08:41 +0100

    username and password before all

--- spec/tumblr_spec.rb
+++ spec/tumblr_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper.rb'
 
-describe "EpicHR Tests" do
+describe "Tumblr Tests" do
+  before :all do
+    @username = "seitgrads16"
+    @password = "t3stacc0unt16"
+  end
   before :each do
     # start an appium session using the desired capabilities set in the spec_helper.rb file
     # launch the appium app
@@ -15,25 +19,10 @@ describe "EpicHR Tests" do
     #driver_quit
   end
 
-  context "Created Alarm" do
-    it "Setting an alarm" do
-      text("Add alarm").click
-      textfields.first.clear
-      textfields.first.type "4"
-      textfields[1].clear
-      textfields[1].type "55"
-      textfields.last.clear
-      textfields.last.type "P"
-      find_element(id: 'button1').click
-      text('Vocal Message').click
-      find_element(id: 'idtext').type "Time to get that ass up!"
-      hide_keyboard
-      find_element(id: 'save').click
-      find_element(id: 'alarm_save').click
-
-
-
-
+  context "Logging in" do
+    it "should allow a valid email address" do
+      find_element(id: 'login_button').click
+      find_element(id: 'email').type ''
     end
   end
 end
