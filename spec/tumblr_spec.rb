c365103b46e6f26e536b13c02482bbdfd4a35131
alexmacaulay <alex@macaulay.me.uk>
2016-10-04 10:45:00 +0100

    logging in passes, email, username and password set up

--- spec/tumblr_spec.rb
+++ spec/tumblr_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper.rb'
 
 describe "Tumblr Tests" do
   before :all do
+    @email = "seitgrads@mailinator.com"
     @username = "seitgrads16"
     @password = "t3stacc0unt16"
   end
@@ -16,13 +17,16 @@ describe "Tumblr Tests" do
 
   after :all do
     # quit the driver after the tests are done
-    #driver_quit
+    driver_quit
   end
 
   context "Logging in" do
     it "should allow a valid email address" do
       find_element(id: 'login_button').click
-      find_element(id: 'email').type ''
+      find_element(id: 'email').type @email
+      find_element(id: 'signup_button').click
+      find_element(id: 'password').type "#{@password}\n"
+      expect(find_element(id: 'topnav_dashboard_button').displayed?).to eq true
     end
   end
 end
